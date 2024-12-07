833b06d0fc23edd81da7d640cadad99fa76077bf
Jesse Holwell <jesse.d.holwell@gmail.com>
2024-12-08 08:43:19 +1100

    day1-2

---
+++ aoc1/part2/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+var arr1 = new List<int>();
+var arr2 = new List<int>();
+var lines = File.ReadAllLines("input.txt");
+var score = 0;
+
+var dictionary = new Dictionary<int, int>();
+
+foreach (var line in lines)
+{
+    var parts = line.Split("   ");
+    dictionary.Add(int.Parse(parts[0]), 0);
+    arr2.Add(int.Parse(parts[1]));
+}
+
+foreach (var item in arr2)
+{
+    if (dictionary.ContainsKey(item))
+        dictionary[item] = dictionary[item] + 1;
+}
+
+foreach (var (key, value) in dictionary)
+{
+    score += key * value;
+}
+
+Console.WriteLine("Similarity score: " + score);
