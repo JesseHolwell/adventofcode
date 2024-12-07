b4f8f1a655f3d52fcfa11a2575ec9824c96d4c1b
Jesse Holwell <jesse.d.holwell@gmail.com>
2024-12-08 08:07:21 +1100

    aoc1

---
+++ aoc1/1-historian-hysteria/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Collections.Immutable;
+
+var arr1 = new List<int>();
+var arr2 = new List<int>();
+var lines = File.ReadAllLines("input.txt");
+var distance = 0;
+
+foreach (var line in lines)
+{
+    var parts = line.Split("   ");
+    arr1.Add(int.Parse(parts[0]));
+    arr2.Add(int.Parse(parts[1]));
+}
+
+arr1.Sort();
+arr2.Sort();
+
+for (var v = 0; v < arr1.Count; v++)
+{
+    distance += Math.Abs(arr1[v] - arr2[v]);
+}
+
+Console.WriteLine("Distance: " + distance);
