90a6d9e82d4020152e29f2d76a06d8f45c1d4a35
Jesse Holwell <jesse.d.holwell@gmail.com>
2024-12-08 19:59:19 +1100

    day2_part2

---
+++ 2/day2/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Hello day 2");
+
+var lines = File.ReadAllLines("input.txt");
+var safeCount = 0;
+
+foreach (var line in lines)
+{
+    var parts = line.Split(" ").Select(int.Parse).ToList();
+
+    bool isSorted = parts.SequenceEqual(parts.OrderBy(x => x));
+
+    var isSafe = isSorted;
+
+    for (var i = 1; i < parts.Count; i++)
+    {
+        if (Math.Abs(parts[i] - parts[i + 1]) > 2)
+        {
+            isSafe = false;
+            return;
+        }
+    }
+
+    if (isSafe)
+    {
+        safeCount++;
+    }
+
+}
+
+Console.WriteLine("Safe count: " + safeCount);
---
+++ 2_2/day2/Program.cs
@@ -0,0 +1,57 @@
+﻿Console.WriteLine("Hello day 2");
+
+var lines = File.ReadAllLines("input.txt");
+var safeCount = 0;
+
+static bool TestList(List<int> list)
+{
+    bool isSorted = list.SequenceEqual(list.OrderBy(x => x)) || list.SequenceEqual(list.OrderByDescending(x => x));
+    if (!isSorted)
+        return false;
+
+    for (int i = 0; i < list.Count - 1; i++)
+    {
+        var difference = Math.Abs(list[i] - list[i + 1]);
+        if (difference > 3 || difference < 1)
+            return false;
+    }
+
+    return true;
+}
+
+static bool TestListWithOmissions(List<int> list)
+{
+    for (int i = 0; i < list.Count; i++)
+    {
+        var templist = list.ToList();
+        templist.RemoveAt(i);
+        var isfixable = TestList(templist);
+        if (isfixable)
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+foreach (var line in lines)
+{
+    var parts = line.Split(" ").Select(int.Parse).ToList();
+
+    var isSafe = TestList(parts);
+
+    if (!isSafe)
+    {
+        isSafe = TestListWithOmissions(parts);
+    }
+
+    if (isSafe)
+    {
+        safeCount++;
+    }
+
+    Console.WriteLine(line + " is " + isSafe);
+}
+
+Console.WriteLine("Safe count: " + safeCount);
